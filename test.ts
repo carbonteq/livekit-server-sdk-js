b455afb44690b5407b3445861ed2761e28198361
Quirk23 <rehan.qureshi@carbonteq.com>
2022-11-28 12:21:20 +0000

    Stop egress after 50 seconds.

--- test.ts
+++ test.ts
@@ -1,31 +1,33 @@
-import {EgressClient, } from "./src/EgressClient"
-import {TrackCompositeEgressRequest,FileAndStreamOutput,StopEgressRequest, EncodedFileType, StreamProtocol} from "./src/proto/livekit_egress"
+import { EgressClient, } from "./src/EgressClient"
+import { FileAndStreamOutput, EncodedFileType, StreamProtocol } from "./src/proto/livekit_egress"
+
+async function main() {
+
+    const egressclient = new EgressClient("ws://localhost:7880", "API7KyKknWj9RVW", "6AKnxnqWWKDXyE8iMM2zffgZYv6Qfm09DuPRVFcHZybB")
+
+    const fileAndStream: FileAndStreamOutput = {
+        fileType: EncodedFileType.MP4,
+        filepath: "ts_test.mp4",
+        urls: ["rtmp://a.rtmp.youtube.com/live2"],
+        protocol: StreamProtocol.RTMP
 
 
-
-async function main(){
-
-const egressclient = new EgressClient("ws://localhost:7880", "API7KyKknWj9RVW", "6AKnxnqWWKDXyE8iMM2zffgZYv6Qfm09DuPRVFcHZybB")
-
-const fileAndStream: FileAndStreamOutput ={
-    fileType:EncodedFileType.MP4,
-    filepath:"ts_test.mp4",
-    urls: ["rtmp://a.rtmp.youtube.com/live2"],
-    protocol:StreamProtocol.RTMP
+    }
 
 
-} 
+    const info = await egressclient.startTrackCompositeEgress("joji", fileAndStream, "TR_AMiJwt5QM43rCr", "TR_VCDZJkdoQffgMY"
+    )
 
+    if (info.error !== "") {
+        console.log("error while connecting egress client")
 
-const info = await egressclient.startTrackCompositeEgress("joji",fileAndStream,"TR_AMiJwt5QM43rCr","TR_VCDZJkdoQffgMY"
-)
+    }
 
-if(info.error!==""){
-    console.log("error while connecting egress client")
+    const egressID = info.egressId;
 
+    setTimeout(async () => {
+        await egressclient.stopEgress(egressID);
+    }, 50000);
 }
 
-const egressID = info.egressId
-
-
-}
+main();
